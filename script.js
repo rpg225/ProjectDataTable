e694b3348a84c3939ddb124cd4835972ce3b4d4b
Rambod Goshtasbi <rpg225@gmail.com>
2024-03-20 23:10:39 -0400

    Created Price multiplier and conversions euro to usd

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ async function loadCards() {
         const EURO_CONVERSION = 0.87;
         let priceMultiplier;
 
+        var cards = cardsJson.map(function(card){
+            var cardsToShow = {
+                "id": card.id,
+                "name": card.name,
+                "priceUSD": card.priceUSD,
+                "isRare": card.isRare
+            }
+
+            return cardsToShow;
+
+        })
+
+        if(inEuro.checked){
+            priceMultiplier = EURO_CONVERSION;
+        } else {
+            priceMultiplier = 1;
+        }
+
+        cardTable.innerHTML = "<tr><td>ID</td><td>Name</td><td>Price</d><td>Is Rare</td>";
+        let rareCards = card.filter(obj => obj.isRare == true).map(obj => ({"id":obj.id, "name":obj.name, "priceUSD": obj.priceUSD, "isRare":obj.isRare}));
+        if (onlyRares.checked) {
+            for (var i = 0; i <  rareCards.length; i++){
+                cardTable.innerHTML += "<tr><td>" + rareCards[i].id + "</td><td>" + rareCards[i].name + "</td><td>" + Math.round(rareCards[i].priceUSD * priceMultiplier) + "</td><td>" + rareCards[i].isRare + "</td></tr>";
+            }
+        } else {
+            for (var i = 0; i < cards.length; i++){
+                cardTable.innerHTML += ""
+            }
+        }
+
         
     } catch(err) {
         alert(err);
